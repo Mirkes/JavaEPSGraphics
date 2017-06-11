eb3b34cafdd3f877e78a033173fe3baa017e4f64
Evgeny Mirkes <em322@le.ac.uk>
2017-06-11 11:06:38 +0100

    Change description
    Change description and add wiki

---
+++ epsgraph/MyEPSException.java
@@ -0,0 +1,17 @@
+package epsgraph;
+/**
+ * This class the special exception for the EPSGrapthics.
+ * @author Evgeny Mirkes (University of Leicester, UK)
+*/
+public class MyEPSException extends RuntimeException {
+    @SuppressWarnings("compatibility:4120342022699198989")
+    private static final long serialVersionUID = 43284893790740006L;
+
+    /**
+     * @param string is the message for the created exception
+     */
+    public MyEPSException(String string) {
+        super(string);
+    }
+
+}
